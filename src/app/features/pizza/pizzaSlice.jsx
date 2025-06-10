c158503c8f59bbec4f7718824cc5550805f612fb
emmeiwhite <emmeiwhite@gmail.com>
2025-06-10 12:48:24 +0530

    2. pizzaSlice for pizza related feature

---
+++ src/app/features/pizza/pizzaSlice.jsx
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  pizzaCount: 30
+}
+const pizzaSlice = createSlice({
+  name: 'pizza',
+  initialState,
+  reducers: {
+    orderPizza: state => {
+      state.pizzaCount--
+    }
+  }
+})
+
+console.log(pizzaSlice)
+// Export action creators and reducer
+export const { pizzaReducer, actions } = pizzaSlice
